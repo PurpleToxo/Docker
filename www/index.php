caf99fe24032412e6c934b73c52571ccb9d5b2ca
Iván Gómez Conde <ivangconde@iessanclemente.net>
2025-09-08 18:53:14 +0200

    Initial commit

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+                echo "<h1>¡Bienvenido al módulo de DWCS 2025-26!</h1>";
+            ?>            
+        </div>
+    </body>
+</html>
