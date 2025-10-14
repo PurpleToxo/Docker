a15175f4be7a1015d507759352f818f7859aad28
Iván <ivan@kubernetes.docker.internal>
2025-10-14 16:16:01 +0200

    Añadidas soluciones UD2

--- www/index.php
+++ www/index.php
@@ -1,14 +1,13 @@
 <html>
     <head>
-        <title>Welcome to LAMP Infrastructure</title>
+        <title>Módulo DWCS</title>
         <meta charset="utf-8">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
-        <div class="container-fluid">
-            <?php
-                echo "<h1>¡Bienvenido al módulo de DWCS 2025-26!</h1>";
-            ?>            
-        </div>
+        <?php
+            echo "<h1>¡Bienvenido al módulo de DWCS 2025-26!</h1>";
+        ?>
     </body>
 </html>
+
