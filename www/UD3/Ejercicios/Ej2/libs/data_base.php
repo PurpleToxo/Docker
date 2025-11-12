338c8c54200b5d14a84ecede318657aa478eb3bf
PurpleToxo <xes.aldea.moreira@outlook.com>
2025-11-12 12:59:29 +0100

    tablas creadas

--- www/UD3/Ejercicios/Ej2/libs/data_base.php
+++ www/UD3/Ejercicios/Ej2/libs/data_base.php
@@ -31,15 +31,34 @@ function create_DB ($connection){
 }
 
 function create_table_donantes($connection){
-
+    $sql="CREATE TABLE IF NOT EXISTS DONANTES(
+    id int (6) auto_increment primary key,
+    nombre varchar (50) not null,
+    apellidos varchar(100) not null
+    edad int (3) not null check (edad>=18),
+    grp_sangre varchar (3) not null(check grp_sangre in ('A+','A-','B+','B-','AB+','AB-','O+','O-')),
+    cod_postal int (5) not null,
+    tlf int (9) not null
+    )";
+    exequteQuery($connection,$sql);
 }
 
 function create_table_historico($connection){
-
+    $sql="CREATE TABLE IF NOT EXISTS HISTORICO(
+    id int (6) auto_increment primary key,
+    id_donante int (6) not null,
+    last_donation date  default current_date,
+    next_donation date generate always as (last_donation + interval 4 months) stored,
+    foreign key (id_donante) references DONANTES(id)
+    )";
+    exequteQuery($connection,$sql);
 }
 
 function create_table_admin($connection){
-
+    $sql ="CREATE TABLE IF NOT EXISTS ADMIN(
+    name_admin varchar (50) primary key,
+    password_admin varchar(200) not null
+    )";
 }
 
 function register_donante(){
