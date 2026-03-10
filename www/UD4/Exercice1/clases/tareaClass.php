ee833184d4c173cf6d146ded7ada47a51ae5b29b
PurpleToxo <xes.aldea.moreira@outlook.com>
2026-03-10 12:42:33 +0100

    update: tarea class 1º step

--- www/UD4/Exercice1/clases/tareaClass.php
+++ www/UD4/Exercice1/clases/tareaClass.php
@@ -3,6 +3,35 @@ class Tarea{
     private $id, $titulo, $descripcion, $estado, $id_usuario;
 
     //setters & getters
-}
+    function setId($id){
+        $this->id=$id;
+    }
+    function getId(){
+        return $this->id;
+    }
+    function setTitulo($titulo){
+        $this->titulo=$titulo;
+    }
+    function getTitulo(){
+        return $this->titulo;
+    }
+    function setDescripcion($descripcion){
+        $this->descripcion=$descripcion;
+    }
+    function getDescripcion(){
+        return $this->descripcion;
+    }
+    function setEstado($estado){
+        $this->estado=$estado;
+    }
+    function getEstado(){
+        return $this->estado;
+    }
+    function setIdUsuario($id_usuario){
+        $this->id_usuario=$id_usuario;
+    }
+    function getIdUsuario(){
+        return $this->id_usuario;
+    }
 
-?>
+}   
